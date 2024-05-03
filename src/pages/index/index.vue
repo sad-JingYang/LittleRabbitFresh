1d841133f8ffbde6a61f09a7839405d23cf65c7a
李景阳 <3306283189@qq.com>
2024-05-03 18:10:11 +0800

    feat:完成了轮播图的指示点

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,7 +3,10 @@ import CustomNavbar from './components/CustomNavbar.vue'
 </script>
 
 <template>
+  <!-- 自定义导航栏 -->
   <CustomNavbar />
+  <!-- 自定义轮播图 -->
+  <XtxSwiper />
   <view class="index">index</view>
 </template>
 
