cdc0745528de4af7ace9221be4bf19ecec2ad86a
李景阳 <3306283189@qq.com>
2024-05-03 22:42:58 +0800

    feat:前台分类组件的封装

--- src/components/components.d.ts
+++
@@ -1,7 +0,0 @@
-import XtxSwiper from '@/components/XtxSwiper.vue'
-
-declare module '@vue/runtime-core' {
-  export interface GlobalComponents {
-    XtxSwiper: typeof XtxSwiper
-  }
-}
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,6 +4,7 @@ import { onLoad } from '@dcloudio/uni-app'
 import { FetchHomeBanner } from '@/services/home'
 import type { BannerItem } from '@/types/home'
 import { ref } from 'vue'
+import CategoryPanel from './components/CategoryPanel.vue'
 
 const bannerList = ref<BannerItem[]>([])
 
@@ -22,9 +23,13 @@ onLoad(() => {
   <CustomNavbar />
   <!-- 自定义轮播图 -->
   <XtxSwiper :list="bannerList" />
+  <!-- 分类面板 -->
+  <CategoryPanel />
   <view class="index">index</view>
 </template>
 
 <style lang="scss">
-//
+page {
+  background-color: #f7f7f7;
+}
 </style>
