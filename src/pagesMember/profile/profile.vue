d0e1a554e2b3a28014bbfc68726f0452664e7acf
李景阳 <3306283189@qq.com>
2024-05-06 01:10:32 +0800

    feat:个人中心-职业修改

--- src/pagesMember/profile/profile.vue
+++ src/pagesMember/profile/profile.vue
@@ -52,7 +52,7 @@ const onAvatarChange = () => {
 
 // 点击保存提交表单
 const onSubmit = async () => {
-  const { nickname, gender, birthday } = profile.value
+  const { nickname, gender, birthday, profession } = profile.value
   const res = await putMemberProfile({
     nickname,
     gender,
@@ -60,6 +60,7 @@ const onSubmit = async () => {
     provinceCode: fullLocationCode[0],
     cityCode: fullLocationCode[1],
     countyCode: fullLocationCode[2],
+    profession,
   })
   // 更新Store昵称
   memberStore.profile!.nickname = res.result.nickname
@@ -160,7 +161,7 @@ onLoad(() => {
         </view>
         <view class="form-item">
           <text class="label">职业</text>
-          <input class="input" type="text" placeholder="请填写职业" :value="profile?.profession" />
+          <input class="input" type="text" placeholder="请填写职业" v-model="profile!.profession" />
         </view>
       </view>
       <!-- 提交按钮 -->
