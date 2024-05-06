7f15ad25b96c0e6d2c9710be9512aaf4919102f6
李景阳 <3306283189@qq.com>
2024-05-07 01:01:23 +0800

    feat:购物车模块-底部结算信息

--- src/pages/cart/cart.vue
+++ src/pages/cart/cart.vue
@@ -66,6 +66,32 @@ const onChangeSelectedAll = () => {
   putMemberCartSelected({ selected: _isSelectedAll })
 }
 
+// 计算选中单品列表
+const selectedCartList = computed(() => {
+  return cartList.value.filter((v) => v.selected)
+})
+
+// 计算选中总件数
+const selectedCartListCount = computed(() => {
+  return selectedCartList.value.reduce((sum, item) => sum + item.count, 0)
+})
+
+// 计算选中总金额
+const selectedCartListMoney = computed(() => {
+  return selectedCartList.value
+    .reduce((sum, item) => sum + item.count * item.nowPrice, 0)
+    .toFixed(2)
+})
+
+// 结算按钮
+const gotoPayment = () => {
+  if (selectedCartListCount.value === 0) {
+    return uni.showToast({ icon: 'none', title: '请选择商品' })
+  }
+  // 跳转到结算页
+  uni.showToast({ title: '等待完成' })
+}
+
 // 初始化调用: 页面显示触发
 onShow(() => {
   if (memeberStore.profile) {
@@ -141,9 +167,11 @@ onShow(() => {
       <view class="toolbar">
         <text class="all" @tap="onChangeSelectedAll" :class="{ checked: isSelectedAll }">全选</text>
         <text class="text">合计:</text>
-        <text class="amount">100</text>
-        <view class="button-grounp">
-          <view class="button payment-button" :class="{ disabled: true }"> 去结算(10) </view>
+        <text class="amount">{{ selectedCartListMoney }}</text>
+        <view class="button-grounp" @tap="gotoPayment">
+          <view class="button payment-button" :class="{ disabled: selectedCartListCount === 0 }">
+            去结算({{ selectedCartListCount }})
+          </view>
         </view>
       </view>
     </template>
